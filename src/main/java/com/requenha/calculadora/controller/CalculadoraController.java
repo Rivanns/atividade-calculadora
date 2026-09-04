6601065aeafef306fd45f6954aad9e109d28342f
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-09-04 16:50:41 -0300

    implementa controller

--- src/main/java/com/requenha/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/requenha/calculadora/controller/CalculadoraController.java
@@ -1,14 +1,38 @@
 package com.requenha.calculadora.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.requenha.calculadora.dto.CalculadoraRequisicao;
+import com.requenha.calculadora.dto.CalculadoraResposta;
+import com.requenha.calculadora.service.CalculadoraService;
 
 @RestController
 public class CalculadoraController {
 	
+	@Autowired
+	private CalculadoraService calculadoraService;
 	
-	//@PostMapping(/calculadora/somar)
-	//public somar () {
-		
-	//}
+	
+	@PostMapping("/calculadora/somar")
+	public CalculadoraResposta somar (@RequestBody CalculadoraRequisicao requisicao) {
+		return calculadoraService.somar(requisicao);
+	}
+	
+	@PostMapping("/calculadora/subtrair")
+	public CalculadoraResposta subtrair(@RequestBody CalculadoraRequisicao requisicao) {
+		return calculadoraService.subtrair(requisicao);
+	}
+	
+	@PostMapping("/calculadora/multiplicar")
+	public CalculadoraResposta multiplicar(@RequestBody CalculadoraRequisicao requisicao) {
+		return calculadoraService.multiplicar(requisicao);
+	}
+	
+	@PostMapping("calculadora/dividir")
+	public CalculadoraResposta dividir(@RequestBody CalculadoraRequisicao requisicao) {
+		return calculadoraService.dividir(requisicao);
+	}
 }
